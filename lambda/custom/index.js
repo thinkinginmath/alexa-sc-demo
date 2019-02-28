b838d589c1074b097305b2190523cd9ff94a301e
Weidong Shao <weidongshao@gmail.com>
2019-02-28 15:15:15 -0800

    Initial code for service center demo

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -8,12 +8,12 @@ const LaunchRequestHandler = {
     return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
   },
   handle(handlerInput) {
-    const speechText = 'Welcome to the Alexa Skills Kit, you can say hello!';
+    const speechText = 'Evergreen Telecom Service Center, what can I do for you?';
 
     return handlerInput.responseBuilder
       .speak(speechText)
       .reprompt(speechText)
-      .withSimpleCard('Hello World', speechText)
+      .withSimpleCard('Service Request', speechText)
       .getResponse();
   },
 };
@@ -21,7 +21,7 @@ const LaunchRequestHandler = {
 const HelloWorldIntentHandler = {
   canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && handlerInput.requestEnvelope.request.intent.name === 'HelloWorldIntent';
+      && handlerInput.requestEnvelope.request.intent.name === 'ServiceAppointment';
   },
   handle(handlerInput) {
     const speechText = 'Hello World!';
@@ -45,6 +45,136 @@ const HelpIntentHandler = {
       .speak(speechText)
       .reprompt(speechText)
       .withSimpleCard('Hello World', speechText)
+      .getResponse();
+  },
+};
+
+
+function httpGet(data, callback) {
+  const http = require('http');
+  const options = {
+    host: '13.52.107.109',
+    path: 'api/send_email',
+    port: 9999,
+    method: 'GET'
+  };
+  var req = http.request(options, function(res) {
+    var responseString = "";
+
+    res.on("data", function (data) {
+        responseString += data;
+        // save all the data from response
+    });
+    res.on("end", function () {
+        console.log(" FROM REST API", responseString); 
+        callback(responseString);
+        // print to console when response ends
+    });
+  });
+
+  req.write(data);
+  req.end();
+}
+function httpPost(data, callback) {
+  const http = require('http');
+  const options = {
+    host: '13.52.107.109',
+    path: 'api/appts',
+    port: 9999,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': data.length
+    }
+  };
+  var req = http.request(options, function(res) {
+    var responseString = "";
+
+    res.on("data", function (data) {
+        responseString += data;
+        // save all the data from response
+    });
+    res.on("end", function () {
+        console.log(" FROM REST API", responseString); 
+        callback(responseString);
+        // print to console when response ends
+    });
+  });
+
+  req.write(data);
+  req.end();
+}
+
+const ChooseServiceTimeHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'ScheduleService';
+  },
+  async handle(handlerInput) {
+    const speechText = 'Your service is confirmed to ';
+    const request = handlerInput.requestEnvelope.request;
+    
+
+    if (request.dialogState === 'IN_PROGRESS') {
+      if (!date.value || !timeOfService.value) {
+         return handlerInput.responseBuilder
+           .addDelegateDirective(request.intent)
+           .getResponse();
+      }
+    } else if (request.dialogState === 'COMPLETED') {
+      var timeOfService = request.intent.slots.serviceTime;
+      var date = request.intent.slots.serviceDay;
+      
+      var dateString = new Date(date.value).toLocaleDateString("en-US", { month: 'short', day: 'numeric', year: 'numeric' });
+      var timeString = timeOfService.value;
+      console.log("request params", dateString, timeString);
+      let outputText = '';
+      var rp = require('request-promise');
+
+      var options = {
+        uri: 'http://nms1.uubright.com/api/apts',
+        method: 'POST',
+        json : true,
+        body : {
+          date: dateString.replace(/,/g, ''),
+          start_time: timeString
+        }
+      };
+  
+      await rp(options).then(function(res) {
+        outputText = '';
+      }).catch(function (err) {
+        // API call fail
+        outputText = "Request to our scheduling server failed. Please try again later";
+      });
+      return handlerInput.responseBuilder
+        .speak(res)
+        .withSimpleCard('Your appointment is set', res)
+        .getResponse();
+    }
+
+    return handlerInput.responseBuilder
+      .speak('There are some internal errors on the server')
+      .withSimpleCard('Your appointment request failed', "failed")
+      .getResponse(); 
+  }   
+},
+};
+
+
+const NetworkStatusIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'NetworkStatus';
+  },
+  handle(handlerInput) {
+    const speechText = 'Network is normal. There are 10 alerts at warning level';
+    httpGet("", function(res) {
+      console.log(res);
+    });
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .withSimpleCard('Network Status', speechText)
       .getResponse();
   },
 };
@@ -97,6 +227,8 @@ exports.handler = skillBuilder
     LaunchRequestHandler,
     HelloWorldIntentHandler,
     HelpIntentHandler,
+    NetworkStatusIntentHandler,
+    ChooseServiceTimeHandler,
     CancelAndStopIntentHandler,
     SessionEndedRequestHandler
   )
